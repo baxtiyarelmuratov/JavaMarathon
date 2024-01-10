405f24b3849b42bb975e12157141c8a7f142c165
Baxtiyar Elmuratov <elmuratovbakhtiyar24@gmail.com>
2024-01-10 21:54:56 +0500

    day1 task_1 to 6

---
+++ src/main/java/day1/Task6.java
@@ -0,0 +1,12 @@
+package day1;
+
+public class Task6 {
+    public static void main(String[] args) {
+        int k = 7;
+        int n = 1;
+        while ( n < 10){
+            System.out.println(n + " * " + k + " = " + n * k);
+            n ++;
+        }
+    }
+}
